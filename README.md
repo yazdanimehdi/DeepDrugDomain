7c1318981c52979e8d8ef5dd5614f3ccc2fe2baa
Mehdi Yazdani <49820319+yazdanimehdi@users.noreply.github.com>
2023-11-25 20:47:35 -0500

    editing the README file to reflect changes

--- README.md
+++ README.md
@@ -14,69 +14,42 @@ DeepDrugDomain is a comprehensive Python toolkit aimed at simplifying and accele
 ## Installation
 For now you can use this environments for usage and development,
 ```bash
-git clone git@github.com:yazdanimehdi/DeepDrugDomain.git
 conda create --name deepdrugdomain python=3.11
 conda activate deepdrugdomain
 conda install -c conda-forge rdkit
-pip install -r requirements.txt
+pip install git+https://github.com/yazdanimehdi/deepdrugdomain.git
 ```
 
 ## Quick Start
 
 ```python
 import deepdrugdomain as ddd
-from deepdrugdomain.models.factory import ModelFactory
-from deepdrugdomain.optimizers.factory import OptimizerFactory
-from deepdrugdomain.schedulers.factory import SchedulerFactory
-from dgllife.utils import CanonicalAtomFeaturizer
 
-feat = CanonicalAtomFeaturizer()
-seed = 4
+data_loaders, model, optimizer, criterion, scheduler, evaluators = ddd.utils.initialize_training_environment(model="supported_model_name", dataset="supported_dataset_by_the_model")
 
-preprocess_drug = ddd.data.PreprocessingObject(attribute="SMILES",
-                                               preprocessing_type="smile_to_dgl_graph",
-                                               preprocessing_settings={"fragment": False, "node_featurizer": feat},
-                                               in_memory=True,
-                                               online=False)
+# setting device on GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-preprocess_protein = ddd.data.PreprocessingObject(attribute="pdb_id",
-                                                  preprocessing_type="protein_pockets_to_dgl_graph",
-                                                  preprocessing_settings={"pdb_path": "data/pdb/", "protein_size_limit": 10000},
-                                                  in_memory=False,
-                                                  online=False)
+epochs = 100
+for i for range(epochs):
+    print(f"Epoch {i}:")
+    # train
+    model.train_one_epoch(data_loaders[0], device, criterion, optimizer, num_epochs=epochs, scheduler=scheduler, evaluator=evaluator[0])
+    # validation
+    print(model.evaluate(data_loaders[1], device, criterion, evaluator=evaluator[1])) 
 
-preprocess_label = ddd.data.PreprocessingObject(attribute="Label",
-                                                preprocessing_type="interaction_to_binary",
-                                                preprocessing_settings={},
-                                                in_memory=True,
-                                                online=True)
-
-preprocesses = preprocess_drug + preprocess_protein + preprocess_label
-
-dataset = ddd.data.DatasetFactory.create("drugbank", file_paths="data/drugbank/", preprocesses=preprocesses)
-
-datasets = dataset(split_method="random_split", frac=[0.8, 0.1, 0.1], seed=seed, sample=0.05)
-
-collate_fn = CollateFactory.create("binding_graph_smile_graph")
-
-data_loader_train = DataLoader(datasets[0], batch_size=32, shuffle=True, num_workers=4, pin_memory=True,
-                                collate_fn=collate_fn, drop_last=True)
-data_loader_val = DataLoader(datasets[1], drop_last=False, batch_size=32,
-                                num_workers=4, pin_memory=False, collate_fn=collate_fn)
-data_loader_test = DataLoader(datasets[2], drop_last=False, batch_size=32, collate_fn=collate_fn,
-                                num_workers=4, pin_memory=False)
-
-model = ModelFactory.create("attentionsitedti")
-
-criterion = torch.nn.BCELoss()
-
-optimizer = OptimizerFactory.create("adamw", model.parameters(), lr=1e-4, weight_decay=0.03)
-scheduler = SchedulerFactory.create("cosine", optimizer)
-
-device = torch.device("cpu")
-model.to(device)
-
+# testing the trained model
+model.evaluate(data_loaders[2], device, criterion, evaluator=evaluator[1]) 
 ```
+## Examples
+
+The [example](./examples/) folder contains a collection of scripts and notebooks demonstrating various capabilities of DeepDrugDomain. Below is an overview of what each example covers:
+
+### Training Different Models
+
+- [attentionsitedti.ipynb](./examples/attentionsitedti.ipynb): Brief explanation of training AttentionSiteDTI with custom configurations and model tampering in this Jupyter Notebook.
+
+### Other Functionalities
 
 ### Documentation
 For now please read the docstring inside the module for more information.
--- deepdrugdomain/utils/training_setup.py
+++ deepdrugdomain/utils/training_setup.py
@@ -1,90 +1,90 @@
-# import os
-# import json
-# import warnings
-# from typing import Any, Dict, List, Optional
-# from deepdrugdomain.data import DatasetFactory
-# from deepdrugdomain.data.collate import CollateFactory
-# from deepdrugdomain.models.factory import ModelFactory
-# from torch.utils.data.dataloader import default_collate
+import os
+import json
+import warnings
+from typing import Any, Dict, List, Optional
+from deepdrugdomain.data import DatasetFactory
+from deepdrugdomain.data.collate import CollateFactory
+from deepdrugdomain.models.factory import ModelFactory
+from torch.utils.data.dataloader import default_collate
 
 
-# def read_config(config_path: str) -> Dict[str, Any]:
-#     """
-#     Reads a JSON configuration file and returns the configuration as a dictionary.
+def read_config(config_path: str) -> Dict[str, Any]:
+    """
+    Reads a JSON configuration file and returns the configuration as a dictionary.
 
-#     Args:
-#         config_path (str): The path to the JSON configuration file.
+    Args:
+        config_path (str): The path to the JSON configuration file.
 
-#     Returns:
-#         Dict[str, Any]: Configuration dictionary with settings for the model and dataset.
-#     """
-#     with open(config_path, 'r') as f:
-#         config = json.load(f)
-#     return config
+    Returns:
+        Dict[str, Any]: Configuration dictionary with settings for the model and dataset.
+    """
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+    return config
 
 
-# def initialize_training_environment(model_name: str, dataset_name: str, split_ratios: List[float], config: Optional[Dict[str, Any]] = None, config_path: Optional[str] = None) -> tuple:
-#     """
-#     Initializes the training environment by loading the model, dataset, and collate function configurations.
-#     It either uses a provided configuration dictionary or loads a configuration from a file. If neither is provided,
-#     it defaults to a configuration file named '{model_name}.json' in the 'deepdrugdomain/configs' directory.
+def initialize_training_environment(model_name: str, dataset_name: str, split_ratios: List[float], config: Optional[Dict[str, Any]] = None, config_path: Optional[str] = None) -> tuple:
+    """
+    Initializes the training environment by loading the model, dataset, and collate function configurations.
+    It either uses a provided configuration dictionary or loads a configuration from a file. If neither is provided,
+    it defaults to a configuration file named '{model_name}.json' in the 'deepdrugdomain/configs' directory.
 
-#     Args:
-#         model_name (str): The name of the model to load.
-#         dataset_name (str): The name of the dataset to load.
-#         split_ratios (List[float]): The ratios to split the dataset into training, validation, and test sets.
-#         config (Optional[Dict[str, Any]]): An optional pre-loaded configuration dictionary.
-#         config_path (Optional[str]): An optional path to the configuration file.
+    Args:
+        model_name (str): The name of the model to load.
+        dataset_name (str): The name of the dataset to load.
+        split_ratios (List[float]): The ratios to split the dataset into training, validation, and test sets.
+        config (Optional[Dict[str, Any]]): An optional pre-loaded configuration dictionary.
+        config_path (Optional[str]): An optional path to the configuration file.
 
-#     Returns:
-#         tuple: A tuple containing the model, dataset splits, and collate function.
+    Returns:
+        tuple: A tuple containing the model, dataset splits, and collate function.
 
-#     Raises:
-#         AssertionError: If certain keys are not present in the configuration or the specified dataset does not match.
-#         FileNotFoundError: If the configuration file does not exist.
-#     """
-#     default_config_path = 'deepdrugdomain/configs'
-#     default_config = False
+    Raises:
+        AssertionError: If certain keys are not present in the configuration or the specified dataset does not match.
+        FileNotFoundError: If the configuration file does not exist.
+    """
+    default_config_path = 'deepdrugdomain/configs'
+    default_config = False
 
-#     # Load configuration from file if not provided
-#     if not config:
-#         if not config_path:
-#             warnings.warn(
-#                 "Using default configuration file located in 'deepdrugdomain/configs'", UserWarning)
-#             config_path = os.path.join(
-#                 default_config_path, f"{model_name}.json")
-#             if not os.path.exists(config_path):
-#                 raise FileNotFoundError(
-#                     f"Config file for model '{model_name}' not found at {config_path}.")
-#             default_config = True
-#         config = read_config(config_path)
+    # Load configuration from file if not provided
+    if not config:
+        if not config_path:
+            warnings.warn(
+                "Using default configuration file located in 'deepdrugdomain/configs'", UserWarning)
+            config_path = os.path.join(
+                default_config_path, f"{model_name}.json")
+            if not os.path.exists(config_path):
+                raise FileNotFoundError(
+                    f"Config file for model '{model_name}' not found at {config_path}.")
+            default_config = True
+        config = read_config(config_path)
 
-#     # Validate configuration
-#     assert 'model' in config, "Configuration file must contain a 'model' key"
-#     assert 'dataset' in config, "Configuration file must contain a 'dataset' key"
-#     assert dataset_name in config['dataset'], f"Dataset '{dataset_name}' does not match any of the datasets specified in the configuration file."
+    # Validate configuration
+    assert 'model' in config, "Configuration file must contain a 'model' key"
+    assert 'dataset' in config, "Configuration file must contain a 'dataset' key"
+    assert dataset_name in config['dataset'], f"Dataset '{dataset_name}' does not match any of the datasets specified in the configuration file."
 
-#     # Create dataset and splits
-#     dataset_config = config['dataset'][dataset_name]
-#     dataset = DatasetFactory.create(dataset_name, **dataset_config)
-#     dataset_splits = dataset.split(split_ratios)
+    # Create dataset and splits
+    dataset_config = config['dataset'][dataset_name]
+    dataset = DatasetFactory.create(dataset_name, **dataset_config)
+    dataset_splits = dataset.split(split_ratios)
 
-#     # Determine if a custom collate function is specified and create it, else use default collate
-#     collate_config = dataset_config.get('collate_fn')
-#     if collate_config:
-#         collate_fn_name = collate_config['name']
-#         collate_fn_args = collate_config.get('args', {})
-#         collate_fn = CollateFactory.create(collate_fn_name, **collate_fn_args)
-#     else:
-#         collate_fn = default_collate
-#         warnings.warn(
-#             "No custom 'collate_fn' specified, using default collate function.", UserWarning)
+    # Determine if a custom collate function is specified and create it, else use default collate
+    collate_config = dataset_config.get('collate_fn')
+    if collate_config:
+        collate_fn_name = collate_config['name']
+        collate_fn_args = collate_config.get('args', {})
+        collate_fn = CollateFactory.create(collate_fn_name, **collate_fn_args)
+    else:
+        collate_fn = default_collate
+        warnings.warn(
+            "No custom 'collate_fn' specified, using default collate function.", UserWarning)
 
-#     # Determine model version and create model
-#     model_version = dataset_config.get('model_version', "default")
-#     if default_config:
-#         model = ModelFactory.create(model_name, version=model_version)
-#     else:
-#         model = ModelFactory.create(model_name, **config['model'])
+    # Determine model version and create model
+    model_version = dataset_config.get('model_version', "default")
+    if default_config:
+        model = ModelFactory.create(model_name, version=model_version)
+    else:
+        model = ModelFactory.create(model_name, **config['model'])
 
-#     return model, dataset_splits, collate_fn
+    return model, dataset_splits, collate_fn
